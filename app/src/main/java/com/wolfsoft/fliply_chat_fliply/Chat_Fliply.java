835b1aa9dc00bc7fa853dd70e7c11c0bc43aaa64
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-15 15:58:23 +0530

    Initial commit

---
+++ app/src/main/java/com/wolfsoft/fliply_chat_fliply/Chat_Fliply.java
@@ -0,0 +1,13 @@
+package com.wolfsoft.fliply_chat_fliply;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chat_Fliply extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat__fliply);
+    }
+}
